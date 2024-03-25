1b7a7624df6f1c6e679618518a2d49375ff30258
dmzapata <dmzapata85@gmail.com>
2024-03-24 18:43:34 -0600

    Mensaje de confirmación

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask, render_template, url_for
 from data import inputs
 
 app = Flask(__name__)
 
+port = int(os.environ.get("PORT", 5000))  # Obtener el puerto de las variables de entorno de Heroku
 
 @app.context_processor
 def inject_dict_for_all_templates():
