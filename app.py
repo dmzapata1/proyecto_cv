b2801720da22e2d6077641469ea4577829dbfb63
dmzapata <dmzapata85@gmail.com>
2024-03-24 18:36:11 -0600

    Mensaje de confirmación

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ from data import inputs
 
 app = Flask(__name__)
 
+# Obtén el puerto desde la variable de entorno, o utiliza el 5000 por defecto
+port = int(os.environ.get("PORT", 5000))
+
 @app.context_processor
 def inject_dict_for_all_templates():
     global_ = {'last name':inputs['last name']}
